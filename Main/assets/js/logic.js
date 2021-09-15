16f5a730ca9173df5d37a5eda6dac8d805e65f65
Desiree Thompson <thompson.desiree@gmail.com>
2021-09-15 15:53:53 -0400

    Adding Code Quiz Project to GitHub

--- Main/assets/js/logic.js
+++ Main/assets/js/logic.js
@@ -12,9 +12,6 @@ var startBtn = document.getElementById("start");
 var initialsEl = document.getElementById("initials");
 var feedbackEl = document.getElementById("feedback");
 
-// sound effects
-var sfxRight = new Audio("assets/sfx/correct.wav");
-var sfxWrong = new Audio("assets/sfx/incorrect.wav");
 
 function startQuiz() {
   // hide start screen
